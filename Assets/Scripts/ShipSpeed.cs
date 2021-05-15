6ecdb5f1f89a83888945311b4213dbee54acbf9d
BigSaladMan <jonathanpauze25@gmail.com>
2021-05-14 22:36:11 -0400

    movement implemented and made better

--- Assets/Scripts/ShipSpeed.cs
+++ Assets/Scripts/ShipSpeed.cs
@@ -7,7 +7,7 @@ public class ShipSpeed: MonoBehaviour
 {
 
 
-    [SerializeField] private float shipAccel = 5f;
+    [SerializeField] private float shipAccel = 7f;
     private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -17,29 +17,52 @@ public class ShipSpeed: MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        //up
-        if (Input.GetAxis("Vertical") > 0 )
+        //quick slowdown if almost stopped
+        if(rb.velocity.magnitude < 1f)
         {
-            rb.AddForce(0,0,shipAccel);
+            rb.AddForce(-rb.velocity.x, 0f, -rb.velocity.z);
+        }
+        //up
+        if (Input.GetAxis("Vertical") > 0f)
+        {
+            if(rb.velocity.magnitude > 20)
+            {
+                rb.AddForce(0f, 0f, 2*shipAccel);
+            }
+            rb.AddForce(0f, 0f, shipAccel);
         }
         //down
-        if (Input.GetAxis("Vertical") < 0)
+        if (Input.GetAxis("Vertical") < 0f)
         {
-            rb.AddForce(0, 0, -shipAccel);
+            if (rb.velocity.magnitude > 20)
+            {
+                rb.AddForce(0f, 0f, -2*shipAccel);
+            }
+            rb.AddForce(0f, 0f, -shipAccel);
         }
         //right
-        if (Input.GetAxis("Horizontal") > 0)
+        if (Input.GetAxis("Horizontal") > 0f)
         {
-            rb.AddForce(0, shipAccel, 0);
+            if (rb.velocity.magnitude > 20)
+            {
+                rb.AddForce(2*shipAccel, 0f, 0f);
+            }
+            rb.AddForce(shipAccel, 0f, 0f);
         }
         //left
-        if (Input.GetAxis("Horizontal") < 0)
+        if (Input.GetAxis("Horizontal") < 0f)
         {
-            rb.AddForce(0, -shipAccel, 0);
+            if (rb.velocity.magnitude > 20)
+            {
+                rb.AddForce(-2*shipAccel, 0f, 0f);
+            }
+            rb.AddForce(-shipAccel, 0f, 0f);
         }
         //capped speed
         rb.velocity = Vector3.ClampMagnitude(rb.velocity, 25f);
+        //friction (20% of speed)
+        rb.AddForce(-rb.velocity.x/5f, 0f, -rb.velocity.z/5f);
     }
 }
