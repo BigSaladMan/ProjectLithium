caa13bac3c4d969632cb7e55bfbe9d33391730cf
BigSaladMan <jonathanpauze25@gmail.com>
2021-05-14 23:36:07 -0400

    random scene management

--- Assets/Scripts/ShipSpeed.cs
+++ Assets/Scripts/ShipSpeed.cs
@@ -24,45 +24,50 @@ public class ShipSpeed: MonoBehaviour
         {
             rb.AddForce(-rb.velocity.x, 0f, -rb.velocity.z);
         }
+        Move();
+        //capped speed
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, 25f);
+        //friction (20% of speed)
+        rb.AddForce(-rb.velocity.x/5f, 0f, -rb.velocity.z/5f);
+    }
+
+    void Move()
+    {
         //up
         if (Input.GetAxis("Vertical") > 0f)
         {
-            if(rb.velocity.magnitude > 20)
+            if (rb.velocity.magnitude > 20f)
             {
-                rb.AddForce(0f, 0f, 2*shipAccel);
+                rb.AddForce(0f, 0f, 2 * shipAccel);
             }
             rb.AddForce(0f, 0f, shipAccel);
         }
         //down
         if (Input.GetAxis("Vertical") < 0f)
         {
-            if (rb.velocity.magnitude > 20)
+            if (rb.velocity.magnitude > 20f)
             {
-                rb.AddForce(0f, 0f, -2*shipAccel);
+                rb.AddForce(0f, 0f, -2 * shipAccel);
             }
             rb.AddForce(0f, 0f, -shipAccel);
         }
         //right
         if (Input.GetAxis("Horizontal") > 0f)
         {
-            if (rb.velocity.magnitude > 20)
+            if (rb.velocity.magnitude > 20f)
             {
-                rb.AddForce(2*shipAccel, 0f, 0f);
+                rb.AddForce(2 * shipAccel, 0f, 0f);
             }
             rb.AddForce(shipAccel, 0f, 0f);
         }
         //left
         if (Input.GetAxis("Horizontal") < 0f)
         {
-            if (rb.velocity.magnitude > 20)
+            if (rb.velocity.magnitude > 20f)
             {
-                rb.AddForce(-2*shipAccel, 0f, 0f);
+                rb.AddForce(-2 * shipAccel, 0f, 0f);
             }
             rb.AddForce(-shipAccel, 0f, 0f);
         }
-        //capped speed
-        rb.velocity = Vector3.ClampMagnitude(rb.velocity, 25f);
-        //friction (20% of speed)
-        rb.AddForce(-rb.velocity.x/5f, 0f, -rb.velocity.z/5f);
     }
 }
